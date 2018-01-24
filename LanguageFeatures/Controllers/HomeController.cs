b63d970099e31344c989a076f3ef767b8ec327b3
Zhenya <zTiff0@gmail.com>
2018-01-24 23:07:37 +0300

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product product = new Product
+            {
+                Name = "Kayak"
+            };
+
+            return View("Result",
+                (object) String.Format("Product name: {0}", product.Name.ToString()));
+        }
+    }
+}
